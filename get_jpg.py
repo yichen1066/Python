239a83e15eeee8cca2f7341a5df792bd5cdaebd5
Yi Chen <847448470@qq.com>
2017-06-08 12:29:53 +0800

    Python Cookbook

--- get_jpg.py
+++ get_jpg.py
@@ -1,19 +1,22 @@
+#!/usr/python3
+
 import re
 import urllib.request
-
-def get_html(url):
-    page = urllib.request.urlopen(url)
-    html = page.read()
+def gethtml(url):
+    page=urllib.request.urlopen(url)
+    html=page.read()
     return html
 
-def get_image(html):
-    html=html.decode('utf-8')
-    reg = r'src="(.+?\.jpg)"'
-    imgre = re.compile(reg)
-
-    imglist = imgre.findall(html)
-    return imglist
-
-html = get_html("https://tieba.baidu.com/p/4895690031")
-
-print(get_image(html))
+def getimg(html):
+    reg = r'src="(.*?\.jpg)"'
+    img=re.compile(reg)
+    bytes.decode('utf-8')
+ #   html=html.encode('utf-8')#python3
+    imglist=re.findall(img,html)
+    x = 0
+    for imgurl in imglist:
+        urllib.request.urlretrieve(imgurl,'%s.jpg'%x)
+        x = x+1
+html=gethtml("https://tieba.baidu.com/p/4889787504")
+#html=gethtml("http://news.ifeng.com/a/20161115/50258273_0.shtml")
+print(getimg(html))
